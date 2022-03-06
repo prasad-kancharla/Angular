793491aee274f6119f8dac7f5dcc2b3701a687cb
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-06 19:48:38 +0530

    add some colors

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: ['p {color: green}'],
 })
 export class ServerComponent {}
