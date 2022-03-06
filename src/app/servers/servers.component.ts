b4b0f8f6e3be44b6cbde86e6f13b3f8e3c30f65e
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-06 22:03:17 +0530

    add two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
-  serverName = '';
+  serverName = 'Tomcat';
 
   serverCreationStatus: String = 'No server is created';
   onServerCreation() {
